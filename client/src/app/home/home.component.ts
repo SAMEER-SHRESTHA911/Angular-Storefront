52f079e3d61385f0560dad666a27e6ae19dc888d
SAMEER SHRESTHA <Nero0293@outlook.com>
2024-08-01 10:05:32 +0545

    Added a product componenet

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../services/products.service';
 import { HttpClientModule } from '@angular/common/http';
-import { Products } from '../../types';
+import { Product, Products } from '../../types';
+import { ProductComponent } from '../components/product/product.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ HttpClientModule],
+  imports: [ HttpClientModule, ProductComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -16,10 +18,14 @@ export class HomeComponent implements OnInit{
 
   }
 
+  products:Product[] = [];
+
   ngOnInit(): void {
     this.productsService.getProducts('http://localhost:3000/clothes',{page:0,perPage:5}).
     subscribe((products:Products) => {
-      console.log(products.items)
+      console.log(products.items);
+      this.products = products.items;
+      console.log(this.products)
     })
   }
 }
